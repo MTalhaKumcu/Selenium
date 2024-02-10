ad91d0537aa3a00a53ce79e2653bff7080e18f89
Mehmet Talha Kumcu <83586203+MTalhaKumcu@users.noreply.github.com>
2024-02-10 19:15:14 +0100

    1and2 class

--- seleniumLearning/src/test/java/Day_01_MakeDriver/C01111_WebDriver.java
+++
@@ -1,6 +0,0 @@
-package Day_01_MakeDriver;
-
-public class C01111_WebDriver {
-
-}
-//"webdriver.chrom.driver","src/resource/chrome.exe"
--- seleniumLearning/src/test/java/Day_01_MakeDriver/C01_WebDriver.java
+++ seleniumLearning/src/test/java/Day_01_MakeDriver/C01_WebDriver.java
@@ -1,5 +1,30 @@
 package Day_01_MakeDriver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class C01_WebDriver {
 
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrom.driver", "src/resource/chrome.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.amazon.com");
+
+        System.out.println(
+                driver.getTitle()+"\n"
+                +driver.getCurrentUrl()
+                );
+        System.out.println(driver.getWindowHandle());
+        // acilan pencerenin unique hash deverini verir
+        System.out.println(driver.getWindowHandles());
+        //ayni islemin birden fazla pencere degerlerinin dondurur
+        System.out.println(driver.getPageSource());
+        //acilan sitenin kaynak kodlarini getirir.
+
+
+
+        Thread.sleep(1000);
+        driver.close();
+    }
 }
