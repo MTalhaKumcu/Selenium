1063723d9bdd543223b587fd56b3a840e84135e1
Mehmet Talha Kumcu <83586203+MTalhaKumcu@users.noreply.github.com>
2024-02-10 18:37:18 +0100

    fisrt class

---
+++ seleniumLearning/src/test/java/Day_01_MakeDriver/C01111_WebDriver.java
@@ -0,0 +1,6 @@
+package Day_01_MakeDriver;
+
+public class C01111_WebDriver {
+
+}
+//"webdriver.chrom.driver","src/resource/chrome.exe"
---
+++ seleniumLearning/src/test/java/Day_01_MakeDriver/C01_WebDriver.java
@@ -0,0 +1,5 @@
+package Day_01_MakeDriver;
+
+public class C01_WebDriver {
+
+}
