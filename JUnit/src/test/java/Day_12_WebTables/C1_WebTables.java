6bd29b91049aa14d528e19fac98e099606579893
Mehmet Talha Kumcu <83586203+MTalhaKumcu@users.noreply.github.com>
2024-03-07 15:46:52 +0100

    webtabler

--- JUnit/src/test/java/Day_12_WebTables/C1_WebTables.java
+++ JUnit/src/test/java/Day_12_WebTables/C1_WebTables.java
@@ -1,4 +1,62 @@
 package Day_12_WebTables;
 
-public class C1_WebTables {
+import Utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.List;
+
+public class C1_WebTables extends TestBase {
+
+    @Test
+    public void test01() throws InterruptedException {
+        driver.get("https://amazon.com");
+
+        Actions actions = new Actions(driver);
+        actions.sendKeys(Keys.END).perform();
+
+        Thread.sleep(5000);
+
+        WebElement allBodyElement = driver.findElement(By.tagName("tbody"));
+        System.out.println(allBodyElement.getText());
+
+        List<WebElement> lineElement = driver.findElements(By.xpath("//tbody/tr"));
+
+        int expectedLineSize = 7;
+        int actualLineSize = lineElement.size();
+        Assert.assertEquals(expectedLineSize, actualLineSize);
+
+        System.out.println("-------");
+
+        for (WebElement eachLine : lineElement) {
+            System.out.println(eachLine.getText());
+            System.out.println("-----------");
+        }
+
+        List<WebElement> firstLineElementList = driver.findElements(By.xpath("//tbody/tr[1]/td"));
+        int expectedColumn = 13;
+        int actualCloumn = firstLineElementList.size();
+        Assert.assertEquals(expectedColumn,actualCloumn);
+
+        System.out.println("=========================");
+        List<WebElement> fifthRowElement = driver.findElements(By.xpath("//tbody/tr/td[5]"));
+        for (WebElement eachRow:fifthRowElement){
+            System.out.println(eachRow.getText());
+            System.out.println("+++++++++++++++");
+        }
+
+        System.out.println("???????????????????????");
+        System.out.println(dataGet(3,5));
+    }
+
+    private String dataGet(int row, int column) {
+        String dynamicXpath = "//tbody/tr["+row+"]/td["+column+"]";
+        WebElement requiredDataElement = driver.findElement(By.xpath(dynamicXpath));
+
+        return requiredDataElement.getText();
+    }
 }
--- JUnit/src/test/java/Utilities/TestBase.java
+++ JUnit/src/test/java/Utilities/TestBase.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -17,6 +18,8 @@ public class TestBase {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
+
+
     }
     @After
     public void teardown(){
