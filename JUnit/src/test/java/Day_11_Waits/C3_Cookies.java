179c623520c4a15e35a8ca7a80e4dd821b98f0c2
Mehmet Talha Kumcu <83586203+MTalhaKumcu@users.noreply.github.com>
2024-03-07 14:12:17 +0100

    cookies

--- JUnit/src/test/java/Day_11_Waits/C3_Cookies.java
+++ JUnit/src/test/java/Day_11_Waits/C3_Cookies.java
@@ -1,12 +1,49 @@
 package Day_11_Waits;
 
 import Utilities.TestBase;
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Cookie;
+
+import java.util.Set;
 
 public class C3_Cookies extends TestBase {
     @Test
-    public void test01(){
+    public void test01() {
         driver.get("https://amazon.com");
+        Set<Cookie> cookieSet = driver.manage().getCookies();
+        int lineNo = 1;
+        for (Cookie eachCookie : cookieSet) {
+            System.out.println(lineNo + "--->" + eachCookie);
+            lineNo++;
+        }
+
+        int expectedMinCookieNumber = 1;
+        int actualCookieNumber = cookieSet.size();
+        Assert.assertEquals(expectedMinCookieNumber, actualCookieNumber);
+     /*
+        String actualCookieValue = driver.manage().getCookieNamed("csm-sid").getValue();
+        String expectedCookieValue = "";
+        Assert.assertEquals(expectedCookieValue, actualCookieValue);
+     */ // var olan cookie`leri gosterir
+
+        Cookie bestCookie = new Cookie("best cookie", "chocolate");
+        driver.manage().addCookie(bestCookie);
+
+        lineNo = 1;
+        cookieSet = driver.manage().getCookies();
+        String bestCookieValue = "";
+
+        for (Cookie eachCookie : cookieSet) {
+            if (eachCookie.getName().equals("best cookie")){
+                bestCookieValue = eachCookie.getValue();
+            }
+                System.out.println(lineNo + "--->" + eachCookie);
+            lineNo++;
+        }
+
+        String expectedCookieValue = "chocolate";
+        Assert.assertEquals(expectedCookieValue,bestCookieValue);
 
     }
 }
