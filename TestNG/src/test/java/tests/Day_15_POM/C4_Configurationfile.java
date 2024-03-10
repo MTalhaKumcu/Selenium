052c51dd2e4c7f1af2c94bdf8a9253636006c9e3
Mehmet Talha Kumcu <83586203+MTalhaKumcu@users.noreply.github.com>
2024-03-10 15:55:43 +0100

    configuration properties and read

---
+++ TestNG/src/test/java/tests/Day_15_POM/C4_Configurationfile.java
@@ -0,0 +1,14 @@
+package tests.Day_15_POM;
+
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class C4_Configurationfile {
+
+
+    @Test
+    public void test01(){
+        Driver.getDriver().get("amazonUrl");
+    }
+
+}
---
+++ TestNG/src/test/java/utilities/ConfigurationReader.java
@@ -0,0 +1,26 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+    static Properties properties;
+
+    static {
+        String filePath = "configuration.properties";
+        try {
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+            properties = new Properties();
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            System.out.println("Configuration.properties is not readable");
+        }
+
+    }
+public static String getProperty(String key){
+        return properties.getProperty(key);
+}
+
+}
