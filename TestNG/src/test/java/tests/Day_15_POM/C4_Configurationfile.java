115fc27fe5460f4f527e142d80cf4a875c3519fc
Mehmet Talha Kumcu <83586203+MTalhaKumcu@users.noreply.github.com>
2024-03-10 16:25:06 +0100

    configuration properties and read

--- TestNG/src/test/java/tests/Day_15_POM/C4_Configurationfile.java
+++ TestNG/src/test/java/tests/Day_15_POM/C4_Configurationfile.java
@@ -1,14 +1,28 @@
 package tests.Day_15_POM;
 
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.ConfigurationReader;
 import utilities.Driver;
 
 public class C4_Configurationfile {
 
 
     @Test
-    public void test01(){
-        Driver.getDriver().get("amazonUrl");
+    public void test01() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("amazonUrl"));
+        AmazonPage amazonPage = new AmazonPage();
+        amazonPage.searchBox.sendKeys(ConfigurationReader.getProperty("amazonSearchText"));
+        amazonPage.searchBox.submit();
+
+        String expectedContent = ConfigurationReader.getProperty("expetedText");
+        String actualContent = amazonPage.result.getText();
+
+        Assert.assertTrue(actualContent.contains(expectedContent));
+
+        Driver.closeDriver();
     }
 
 }
