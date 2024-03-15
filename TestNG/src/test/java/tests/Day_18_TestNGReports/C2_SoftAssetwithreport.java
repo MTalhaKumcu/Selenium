caaf5963297a7d305e83ed996000ce0c467d0497
Mehmet Talha Kumcu <83586203+MTalhaKumcu@users.noreply.github.com>
2024-03-15 10:45:44 +0100

    multi test report with xml

--- TestNG/src/test/java/tests/Day_18_TestNGReports/C2_SoftAssetwithreport.java
+++ TestNG/src/test/java/tests/Day_18_TestNGReports/C2_SoftAssetwithreport.java
@@ -1,11 +1,51 @@
 package tests.Day_18_TestNGReports;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+import utilities.TestBaseReport;
 
-public class C2_SoftAssetwithreport {
+public class C2_SoftAssetwithreport extends TestBaseReport {
+
     @Test
-    public void test01() {
+    public void amazonPage() {
+        extentTest = extentReports.createTest("amazon Test");
 
+        Driver.getDriver().get(ConfigurationReader.getProperty("amazonUrl"));
+        String expectedText = ConfigurationReader.getProperty("amazonTitle");
+        String actualText = Driver.getDriver().getCurrentUrl();
+        extentTest.info("amazon title test ");
+        Assert.assertTrue(actualText.contains(expectedText));
+        extentTest.info("test passed");
+        Driver.quitDriver();
+        extentTest.info("quit");
+    }
 
+    @Test
+    public void wiseQuarter() {
+        extentTest = extentReports.createTest("wise quarter test");
+        Driver.getDriver().get(ConfigurationReader.getProperty("wiseUrl"));
+        String expectedText = ConfigurationReader.getProperty("wiseTitle");
+        String actualText = Driver.getDriver().getCurrentUrl();
+        extentTest.info("test made for wisequarter");
+        Assert.assertTrue(actualText.contains(expectedText));
+        extentTest.pass("wise test passed");
+        Driver.closeDriver();
+        extentTest.info("quit");
+    }
+
+    @Test
+    public void facebookTitle() {
+        extentTest = extentReports.createTest("facebook test");
+        Driver.getDriver().get(ConfigurationReader.getProperty("facebookUrl"));
+        String expectedText = ConfigurationReader.getProperty("facebookTitle");
+        String actualText = Driver.getDriver().getCurrentUrl();
+        extentTest.info("facebook title test");
+        Assert.assertTrue(actualText.contains(expectedText));
+        extentTest.info("facebook test passed");
+        Driver.quitDriver();
+        extentTest.info("quit");
     }
 }
+
