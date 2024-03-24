b125545677d9abb9d71e8771263c44868b1b91ab
Mehmet Talha Kumcu <83586203+MTalhaKumcu@users.noreply.github.com>
2024-03-24 14:58:13 +0100

    parallel test

---
+++ TestNG/src/test/java/tests/Day_18_TestNGReports/C3_DataProvider.java
@@ -0,0 +1,25 @@
+package tests.Day_18_TestNGReports;
+
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+
+public class C3_DataProvider {
+
+    @Test
+    public void test01() {
+
+        Driver.getDriver().get(ConfigurationReader.getProperty("amazonUrl"));
+        AmazonPage amazonPage = new AmazonPage();
+        amazonPage.searchBox.sendKeys(ConfigurationReader.getProperty("neededWord")+ Keys.ENTER);
+        String actualText  = amazonPage.result.getText();
+        String expectedText = ConfigurationReader.getProperty("expetedText");
+
+
+        Assert.assertTrue(actualText.contains(expectedText));
+        Driver.quitDriver();
+    }
+}
