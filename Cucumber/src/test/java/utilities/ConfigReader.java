b1f8c70b841fc1ed56d23cde89c7b327a8d62af8
Mehmet Talha Kumcu <83586203+MTalhaKumcu@users.noreply.github.com>
2024-03-25 21:43:23 +0100

    cucumber

---
+++ Cucumber/src/test/java/utilities/ConfigReader.java
@@ -0,0 +1,27 @@
+package utilities;
+
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    static Properties properties;
+
+    static {
+        String filePath = "configuration.propeties";
+        try {
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+            properties = new Properties();
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            System.out.println("Configuration.properties is not readable");
+        }
+    }
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+}
