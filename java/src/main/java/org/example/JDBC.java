14267fa69ca41dba27c5818955459e60fdfa8d31
MTalhaKumcu <mkumcu@gmail.com>
2024-04-16 17:22:04 +0200

    JDBC required steps

--- java/src/main/java/org/example/JDBC.java
+++ java/src/main/java/org/example/JDBC.java
@@ -5,19 +5,19 @@ import java.sql.*;
 public class JDBC {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-            //1.STEP
+        //1.STEP
         Class.forName("com.mysql.cj.jdbc,Driver");
-            //2.STEP
+        //2.STEP have to
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/java", "root", "Admin");
-            //3.STEP
-        String query = "SELECT userlastname FROM users";
+        //3.STEP have to
+        String query = "SELECT * FROM users";
 
         Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            //4.STEP
-        statement.executeQuery(query);
-
-
-
+        //4.STEP have to
+        ResultSet resultSet = statement.executeQuery(query);
+        //5.STEP
+        resultSet.next();//Iterator
+        resultSet.getString("username");//
     }
 
 }
