33caa4e9810cef3fae84191ee63b4e1753637baa
MTalhaKumcu <mkumcu@gmail.com>
2024-04-26 13:14:03 +0200

    selenium grid

--- Selenium-Grid/src/test/java/stepDefinitions/Grid_03.java
+++ Selenium-Grid/src/test/java/stepDefinitions/Grid_03.java
@@ -1,4 +1,35 @@
 package stepDefinitions;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+import static manage.DriverManage.*;
+
 public class Grid_03 {
+
+
+    @BeforeTest
+    @Parameters("browser")
+    void beforeTest(@Optional ("chrome") String browser){
+        setDriver(browser);
+    }
+
+
+    @Test
+    public void Test01(){
+        driver.get("https://youtube.com");
+        System.out.println(driver.getTitle());
+
+    }
+
+    @AfterTest
+    void afterTest01(){
+        closeDriver();
+    }
+
 }
+
