6772fda8908337ea489f0391dfc34f85cb8b12a5
Mtalhakumcu <mkumcu151@gmail.com>
2024-04-29 18:24:38 +0200

    instagram factorypage added

--- jenkinsTest/src/test/java/pages/instagram_Page.java
+++ jenkinsTest/src/test/java/pages/instagram_Page.java
@@ -1,9 +1,14 @@
 package pages;
 
+
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class instagram_Page {
 
-    PageFactory.initElements(Driver.getDriver(), this);
+    public instagram_Page() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+
 }
