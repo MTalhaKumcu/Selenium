e3e2592195f3cf5e4805c5bcd7aa9e42f523540d
Mtalhakumcu <mkumcu151@gmail.com>
2024-05-19 15:49:16 +0200

    response jsonObject

--- APITestWithSelenium/src/test/java/Test/C03_JSONObjectsCreation.java
+++ APITestWithSelenium/src/test/java/Test/C03_JSONObjectsCreation.java
@@ -3,6 +3,8 @@ package Test;
 import org.json.JSONObject;
 import org.junit.Test;
 
+import java.security.PublicKey;
+
 public class C03_JSONObjectsCreation {
 
 
@@ -13,7 +15,27 @@ public class C03_JSONObjectsCreation {
         jsonObject.put("body", "Merhaba");
         jsonObject.put("userId", 1);
 
-        System.out.println("json obj ==> "+jsonObject);
+        System.out.println("json obj ==> " + jsonObject);
+    }
+
+    //Nested JSON Object
+    @Test
+    public void jsonObj02() {
+        JSONObject innerJson = new JSONObject();
+        innerJson.put("checkin", "2018-01-01");
+        innerJson.put("checkout", "2019-01-01");
+
+        JSONObject outherJson = new JSONObject();
+        outherJson.put("firstname", "Jim");
+        outherJson.put("additionalneeds", "Breakfast");
+        outherJson.put("bookingdates", innerJson);
+        outherJson.put("totalprice", 111);
+        outherJson.put("depositpaid", true);
+        outherJson.put("lastname", "Brown");
+
+        System.out.println("outher Json" +outherJson);
+
+
     }
 
 }
