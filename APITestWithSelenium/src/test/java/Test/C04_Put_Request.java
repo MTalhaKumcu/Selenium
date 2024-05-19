444eede7916800721c600283dc6ec8dfb2a3ac8a
Mtalhakumcu <mkumcu151@gmail.com>
2024-05-19 16:30:36 +0200

    put response

---
+++ APITestWithSelenium/src/test/java/Test/C04_Put_Request.java
@@ -0,0 +1,32 @@
+package Test;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C04_Put_Request {
+
+
+    @Test
+    public void putAssertionTest01() {
+        String url = "https://jsonplaceholder.typicode.com/posts/70";
+
+
+        JSONObject reqBody = new JSONObject();
+        reqBody.put("title", "Ahmet");
+        reqBody.put("body", "Merhaba");
+        reqBody.put("userId", 10);
+        reqBody.put("id", 70);
+
+        Response response = given()
+                                .contentType(ContentType.JSON)
+                           .when()
+                                .body(reqBody.toString()).put(url);
+        response.prettyPrint();
+
+
+    }
+}
