b8c2eddce90d1add9950411f413188b69b0eed12
Mtalhakumcu <mkumcu151@gmail.com>
2024-05-19 15:03:32 +0200

    response Methods

--- APITestWithSelenium/src/test/java/Test/C01_Get_Api_Query.java
+++ APITestWithSelenium/src/test/java/Test/C01_Get_Api_Query.java
@@ -14,5 +14,11 @@ public class C01_Get_Api_Query {
         Response response = given().when().get(url);
         //restassured lib
         response.prettyPrint();
+        System.out.println("status code "+response.getStatusCode());
+        System.out.println("Content Type "+response.getContentType());
+        System.out.println("Header "+response.getHeader("server"));
+        System.out.println("Status Line "+response.getStatusLine());
+        System.out.println("Response Time "+response.getTime());
+
     }
 }
