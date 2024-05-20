952ea64458c8e1482ebc3ed9aaf445450f7021d1
Mtalhakumcu <mkumcu151@gmail.com>
2024-05-20 15:28:19 +0200

    get request test with matchers method

---
+++ APITestWithSelenium/src/test/java/Test/C07_GET_Rep.java
@@ -0,0 +1,31 @@
+package Test;
+
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C07_GET_Rep {
+
+
+    @Test
+    public void getRepTest01() {
+
+        String url = "https://restful-booker.herokuapp.com/booking/10";
+
+
+        Response response = given().when().get(url);
+
+        response.prettyPrint();
+
+        response.then().assertThat()
+                .statusCode(200)
+                .contentType("application/json; charset=utf-8")
+                .body("firstname" , Matchers.equalTo("Mary"),
+                        "lastname", Matchers.equalTo("Wilson"),
+                        "totalprice", Matchers.equalTo(928));
+    }
+
+
+}
