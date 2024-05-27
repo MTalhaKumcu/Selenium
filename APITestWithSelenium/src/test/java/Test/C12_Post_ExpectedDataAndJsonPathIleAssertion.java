e96c2fdd7f2e809dda71371c9831e5ec874d9b1e
Mtalhakumcu <mkumcu151@gmail.com>
2024-05-27 14:55:37 +0200

    expected data and junit assertion and jsonpath

--- APITestWithSelenium/src/test/java/Test/C12_Post_ExpectedDataAndJsonPathIleAssertion.java
+++ APITestWithSelenium/src/test/java/Test/C12_Post_ExpectedDataAndJsonPathIleAssertion.java
@@ -1,10 +1,14 @@
 package Test;
 
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class C12_Post_ExpectedDataAndJsonPathIleAssertion {
 
@@ -21,27 +25,34 @@ public class C12_Post_ExpectedDataAndJsonPathIleAssertion {
 
         JSONObject reqBody = new JSONObject();
 
-        reqBody.put("firstname","ahmet");
-        reqBody.put("lastname","Bulut");
-        reqBody.put("totalprice",500);
-        reqBody.put("depositpaid",false);
-        reqBody.put("bookingdates",bookingDate);
-        reqBody.put("additionalneeds","wi-fi");
+        reqBody.put("firstname", "ahmet");
+        reqBody.put("lastname", "Bulut");
+        reqBody.put("totalprice", 500);
+        reqBody.put("depositpaid", false);
+        reqBody.put("bookingdates", bookingDate);
+        reqBody.put("additionalneeds", "wi-fi");
 
         //2- expected data
 
         JSONObject expecteDate = new JSONObject();
-        expecteDate.put("bookingfid",24);
+        expecteDate.put("bookingfid", 24);
         expecteDate.put("booking", reqBody);
 
 
         //3- response
+        Response response = given().contentType(ContentType.JSON)
+                .when()
+                .body(reqBody.toString())
+                .post(url);
 
-
+        response.prettyPrint();
 
         //4- assertion
 
+        JsonPath respJsonPath = response.jsonPath();
 
+        assertEquals(expecteDate.getJSONObject("booking").get("firstname"),
+                     respJsonPath.get("booking.firstname"));
 
     }
 }
