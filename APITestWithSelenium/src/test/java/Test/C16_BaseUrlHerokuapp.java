b1cd452fd000aca226054298f021bb4a491333dc
Mtalhakumcu <mkumcu151@gmail.com>
2024-05-31 13:23:23 +0200

    exercise

--- APITestWithSelenium/src/test/java/Test/C16_BaseUrlHerokuapp.java
+++ APITestWithSelenium/src/test/java/Test/C16_BaseUrlHerokuapp.java
@@ -1,4 +1,24 @@
 package Test;
 
-public class C16_BaseUrlHerokuapp {
+import BaseUrl.HerokuAppBaseUrl;
+import org.junit.Test;
+
+public class C16_BaseUrlHerokuapp extends HerokuAppBaseUrl {
+
+    @Test
+    public void test01() {
+        //1-url
+        //2-expected
+        //3-response
+        //4-assertion
+
+    }
+    @Test
+    public void test02() {
+        //1-url
+        //2-expected
+        //3-response
+        //4-assertion
+
+    }
 }
--- APITestWithSelenium/src/test/java/Test/C17_BaseUrlHerokuappQueryParam.java
+++ APITestWithSelenium/src/test/java/Test/C17_BaseUrlHerokuappQueryParam.java
@@ -1,7 +1,46 @@
 package Test;
 
-public class C17_BaseUrlHerokuappQueryParam {
+import BaseUrl.HerokuAppBaseUrl;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C17_BaseUrlHerokuappQueryParam extends HerokuAppBaseUrl {
 
 
+    @Test
+    public void get01() {
+        //1-url
+        specHerokuApp.pathParam("pp1", "booking");
+
+        //2-expected
+        //3-response
+        Response response = given().spec(specHerokuApp)
+                .when().get("/{pp1}");
+
+        response.prettyPrint();
+        //4-assertion
+        response.then().assertThat().statusCode(200)
+                .body("bookingid", Matchers.hasItem(88));
+
+    }
+
+    @Test
+    public void get02() {
+        //1-url
+        //2-expected
+        //3-response
+        //4-assertion
+    }
+
+    @Test
+    public void get03() {
+        //1-url
+        //2-expected
+        //3-response
+        //4-assertion
+    }
 
 }
